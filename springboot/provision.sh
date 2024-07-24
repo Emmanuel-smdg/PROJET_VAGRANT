20d9efae3b23faac0273717d72be241106688bcd
lariseve <larissa0182002@gmail.com>
2024-07-24 21:07:17 +0200

    springboot

---
+++ springboot/provision.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Mise à jour des paquets
+sudo apt-get update
+
+# Installation de Java et Maven
+sudo apt-get install -y openjdk-8-jdk maven
+
+# Compilation et lancement de l'application Spring Boot
+cd /home/vagrant/banking_App
+mvn clean package
+nohup java -jar target/banking_App-0.0.1-SNAPSHOT.jar -DskipTests &
